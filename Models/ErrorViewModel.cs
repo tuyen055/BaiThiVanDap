cd9624543b1fe4d2a3f25f0e54b04e63b9ee3c53
tuyen055 <142509972+tuyen055@users.noreply.github.com>
2024-01-03 21:35:38 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoanThiTuyen_039.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
